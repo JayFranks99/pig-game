05c416d6e5ee99ffda4c18fa8046c656ccb82875
Jay Franks <jay@healthystuff.com>
2022-04-04 17:51:55 +0100

    hold function nearly there

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// TESTING COMIT
-
 // TASKS
 
 // ROLLING DICE (roll click function)
@@ -31,6 +29,7 @@ const player2 = {
 };
 
 const rollBtn = document.querySelector(".btn--roll");
+const holdBtn = document.querySelector(".btn--hold");
 
 let diceImg = document.querySelector(".dice");
 
@@ -42,10 +41,8 @@ let p2Score = document.querySelector("#score--2");
 p2Score.textContent = player2.score;
 let p2CurrentScore = document.querySelector("#current--2");
 
-const p1Active = document.querySelector("player--1");
-const p2Active = document.querySelector("player--2");
-
-console.log(diceImg);
+let p1Active = document.querySelector(".player--1");
+let p2Active = document.querySelector(".player--2");
 
 console.log(player1, player2);
 
@@ -55,35 +52,71 @@ function randomIntFromInterval(min, max) {
 }
 
 rollBtn.addEventListener("click", function () {
-  //   console.log("Roll button clicked");
   let rollNumber = randomIntFromInterval(1, 6);
   console.log(`Roll is ${rollNumber}`);
+
   let randomDiceImage = "dice-" + rollNumber + ".png";
   diceImg.setAttribute("src", randomDiceImage);
 
   // Check if player 1 is active (is by default)
   if (player1.currentPlayer === true) {
-    // If 1 is rolled, switch player to player 2
+    // If 1 is rolled, switch player 1 to player 2
     if (rollNumber === 1) {
       player2.currentPlayer = true;
       player1.currentPlayer = false;
+      p2Active.classList.add("player--active");
+      p1Active.classList.remove("player--active");
+      player1.score = 0;
+      p1Score.textContent = player1.score;
     } else {
       // Add roll number to player1 current score
       player1.score = rollNumber + player1.score;
       // Update the website with the player 1 current score
       p1Score.textContent = player1.score;
+      console.log(player1.score, player2.score);
     }
   } else if (player2.currentPlayer === true) {
-    // If 1 is rolled, switch player to player 1
+    // If 1 is rolled, switch player 2 to player 1
     if (rollNumber === 1) {
       player1.currentPlayer = true;
       player2.currentPlayer = false;
+      p2Active.classList.remove("player--active");
+      p1Active.classList.add("player--active");
+      player2.score = 0;
+      p2Score.textContent = player2.score;
     } else {
       // Add roll number to player1 current score
       player2.score = rollNumber + player2.score;
       // Update the website with the player 1 current score
       p2Score.textContent = player2.score;
+      console.log(player1.score, player2.score);
     }
+  }
+});
+
+// HOLD FUNCTIOn
+
+holdBtn.addEventListener("click", function () {
+  console.log("clicked");
+  console.log(player1.score, player2.score);
+  if (player1.currentPlayer === true) {
+    player1.currentScore = player1.score;
+    p1CurrentScore.textContent = player1.currentScore;
+    player1.score = 0;
+    p1Score.textContent = player1.score;
+    player2.currentPlayer = true;
+    player1.currentPlayer = false;
+    p2Active.classList.add("player--active");
+    p1Active.classList.remove("player--active");
+  } else if (player2.currentPlayer === true) {
+    player2.currentScore = player2.score;
+    p2CurrentScore.textContent = player2.currentScore;
+    player2.score = 0;
+    p2Score.textContent = player2.score;
+    player1.currentPlayer = true;
+    player2.currentPlayer = false;
+    p2Active.classList.remove("player--active");
+    p1Active.classList.add("player--active");
   }
 });
 
